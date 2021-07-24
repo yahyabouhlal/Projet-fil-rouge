fbd4344ae41a1e155279daca09c851ad5591bc89
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2021-07-24 13:49:05 +0000

    creatRepository

---
+++ Ecom/Ecom/Repository/CategorySerice.cs
@@ -0,0 +1,48 @@
+﻿using Ecom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecom.Repository
+{
+    public class CategorySerice : ICategorySerice
+    {
+        private readonly ProductContext _context;
+
+        public CategorySerice(ProductContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategory(Category category)
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategory(Category category, Category dbCategory)
+        {
+            dbCategory.Name = category.Name;
+            dbCategory.Url = category.Url;
+            dbCategory.Icon = category.Icon;
+            await _context.SaveChangesAsync();
+        }
+        public async Task<List<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryByUrl(string categoryUrl)
+        {
+            return await _context.Categories.
+             FirstOrDefaultAsync(c => c.Url.ToLower().Equals(categoryUrl.ToLower()));
+        }
+    }
+}
---
+++ Ecom/Ecom/Repository/ICategorySerice.cs
@@ -0,0 +1,18 @@
+﻿using Ecom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecom.Repository
+{
+    interface ICategorySerice
+    {
+        Task<List<Category>> GetCategories();
+        Task<Category> GetCategoryByUrl(string categoryUrl);
+        Task CreateCategory(Category category);
+        Task DeleteCategory(Category category);
+        Task UpdateCategory(Category category, Category dbCategory);
+    }
+}
+}
